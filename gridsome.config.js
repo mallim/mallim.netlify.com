3800f378b09c74446ff524eca213ab4b27f0c357
Ian Lim <mallim.ink@gmail.com>
2019-06-08 23:59:56 +0800

    ready with Bleda

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,57 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Mallim",
+  titleTemplate: `%s | Mallim`,
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      plugins: [
+        [
+          "gridsome-plugin-remark-shiki",
+          {
+            theme: "min-light"
+          }
+        ]
+      ]
+    }
+  },
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/posts/**/*.md",
+        typeName: "Post",
+        route: "/:slug",
+        refs: {
+          tags: {
+            typeName: "Tag",
+            route: "/tag/:id",
+            create: true
+          },
+          author: {
+            typeName: "Author",
+            route: "/author/:id",
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: "gridsome-plugin-tailwindcss",
+      options: {
+        tailwindConfig: "./tailwind.config.js",
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true
+      }
+    }
+  ]
+};
